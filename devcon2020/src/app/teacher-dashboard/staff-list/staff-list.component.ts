5ead416b52ea433fdf9f43926613d01a91902a44
christopher.fernandes <christyabcd.05@gmail.com>
2020-02-16 00:58:26 +0530

    Staff-list page added

--- devcon2020/src/app/teacher-dashboard/staff-list/staff-list.component.ts
+++ devcon2020/src/app/teacher-dashboard/staff-list/staff-list.component.ts
@@ -6,10 +6,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff-list.component.scss']
 })
 export class StaffListComponent implements OnInit {
+  public defaultProfileImg = './assets/images/pngfind.com-person-icon-png-532960.png';
+  public  teacherList = [
+    {
+      name: 'Jo Ann Fernandes',
+      subjects: ['class 10', 'class 6'],
+      image: 'https://pngdrive.com/wp-content/uploads/edd/2019/08/pngdrive-1000x1000-Teacher-27.jpg',
+      selected: false
+    },
+    {
+      name: 'Kate Winslet',
+      subjects: ['class 10', 'class 6'],
+      image: 'https://images.iphonephotographyschool.com/24755/portrait-photography.jpg',
+      selected: false
+    },
+    {
+      name: 'Celine Dion',
+      subjects: ['class 10', 'class 6'],
+      image: 'https://i.pinimg.com/originals/9e/c4/a7/9ec4a7d81442d0183cf332ce959dc310.jpg',
+      selected: false
+    },
+    {
+      name: 'Taylor Swift',
+      subjects: ['class 10', 'class 6'],
+      image: 'https://g2.ravendev.lt/wp-content/uploads/2019/05/100-Beautiful-girls-wallpapers-55-2.jpg',
+      selected: true
+    },
+    {
+      name: 'Angelina Julie',
+      subjects: ['class 10', 'class 6'],
+      image: 'https://kelicommheadshots.com/wp-content/uploads/2019/02/Jen-for-Social-Media-2.jpg',
+      selected: false
+    },
+    {
+      name: 'Asha Kiran ',
+      subjects: ['class 10', 'class 6'],
+      image: 'https://odishatv.in/wp-content/uploads/2017/08/teachers.png',
+      selected: false
+    },
+    {
+      name: 'Emma Watson',
+      subjects: ['class 10', 'class 6', 'class 8'],
+      image: '',
+      selected: false
+    },
+    {
+      name: 'Jo Ann Fernandes',
+      subjects: ['class 10', 'class 6', 'class 8', 'class 9'],
+      image: 'https://g2.ravendev.lt/wp-content/uploads/2019/05/100-Beautiful-girls-wallpapers-55-2.jpg',
+      selected: false
+    },
+    {
+      name: 'Emma Watson',
+      subjects: ['class 10', 'class 6', 'class 8'],
+      image: 'https://images.iphonephotographyschool.com/24755/portrait-photography.jpg',
+      selected: false
+    },
+    {
+      name: 'Emma Watson',
+      subjects: ['class 10', 'class 6', 'class 8'],
+      image: 'https://i.pinimg.com/originals/9e/c4/a7/9ec4a7d81442d0183cf332ce959dc310.jpg',
+      selected: false
+    }
+
+    
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toogleSelected(teacher) {
+    this.teacherList.map( t => {
+      t.selected = false;
+    });
+    teacher.selected = !teacher.selected;
   }
 
 }
